35028b7ba9aac2112880f9effa0189e2a435f795
Xinophobia <blessxino@gmail.com>
2023-02-16 10:54:07 -0800

    Printing stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 
 /**
  * main - prints "and that piece of art is useful - Dora Korpar, 2015-10-19"
- * Return: always 1
+ * Return: always 0
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+	printf( stderr "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
 }
