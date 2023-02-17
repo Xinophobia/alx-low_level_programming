85832328a0ffff26fa81543595f6882a03eb2d11
Xinophobia <blessxino@gmail.com>
2023-02-17 06:23:49 -0800

    Printing the letters of the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints all the alphabets in lowercase
+ * Return: 0
+ */
+
+char main(void)
+{
+	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = a; i < 26; i++)
+	{
+		putchar(alpha[i]);
+	}
+	putchar('\n')
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,2 +1,3 @@
 To know if n is postive or negative
 Printing the last digit
+Printing the letters of the alphabet
