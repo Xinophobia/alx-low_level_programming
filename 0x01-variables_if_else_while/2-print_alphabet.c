5e2ec902bbee83efb14bf620cf21fc7f00c85cda
Xinophobia <blessxino@gmail.com>
2023-02-17 06:27:06 -0800

    Printing the letters of the alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,10 +9,10 @@ char main(void)
 	char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
-	for (i = a; i < 26; i++)
+	for (i = 0; i < 26; i++)
 	{
 		putchar(alpha[i]);
 	}
-	putchar('\n')
+	putchar('\n');
 	return (0);
 }
