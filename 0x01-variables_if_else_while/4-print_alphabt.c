68d7ea6ada46255d0f1b126f8527739b8395eec9
Xinophobia <blessxino@gmail.com>
2023-02-17 07:14:23 -0800

    Printing the alphabets except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints letters of the alphabet except q and e
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alpha[24] = "abcdfghijklmnoprstuvwxyz";
+	int i;
+
+	for (i = 0; i < 24; i++)
+	{
+		putchar(alpha[i]);
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -2,3 +2,4 @@ To know if n is postive or negative
 Printing the last digit
 Printing the letters of the alphabet in lowercase
 Printing the letters of the alphabet in lowercase and uppercase
+Printing the alphabets except e and q
